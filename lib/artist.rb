55a6001357b122ee069f274b564ddff0650825ed
Paul Ashour <paul.ashour@gmail.com>
2018-11-20 16:18:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,17 @@ class Artist
   
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  ########### Class methods ############
+  
+  def self.all
+    @@all
   end
   
 end
