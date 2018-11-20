ebf9dd7a24bcddcbf76042dad13b2a0454332a22
Paul Ashour <paul.ashour@gmail.com>
2018-11-20 16:29:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,7 @@ class Artist
   end
   
   def new_song(name, genre)
-    new_song = Song.new(name)
-    new_song.set_genre(genre)
-    new_song.set_artist(self)
+    new_song = Song.new(name, self, genre)
     @songs << new_song
     new_song
   end
