7b122468dd43f55a1165214b5ce8795db0c91f82
Paul Ashour <paul.ashour@gmail.com>
2018-11-20 21:01:43 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,2 +1,24 @@
 class Patient
+  attr_accessor :appointments
+  attr_reader :name
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
+  end
+  
+  def new_appointment(date, doctor)
+    new_app = Appointment.new(doctor, self, date)
+    doctor.appointments << new_app
+  end
+  
+  ########### Class methods ############
+  
+  def self.all
+    @@all
+  end
+  
+end
