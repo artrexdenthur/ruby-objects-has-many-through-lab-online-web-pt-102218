d71735ecd9fe1eaceeddd8493f87951312038701
Paul Ashour <paul.ashour@gmail.com>
2018-11-20 16:38:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,18 @@ class Song
   
   attr_reader :name, :artist, :genre
   
+  @@all = []
+  
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@all << self
   end
   
+  ########### Class methods ############
   
+  def self.all
+    @@all
+  end
 end
